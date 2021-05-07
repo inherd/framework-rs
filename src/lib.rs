fb59ad59cde6db231ca4dfbc127434291b9817ec
Phodal Huang <h@phodal.com>
2021-05-07 11:35:50 +0800

    fix: fix export issue

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,21 @@
 
 #[macro_use]
 extern crate lazy_static;
-
 #[macro_use]
 extern crate maplit;
-
-#[macro_use]
-extern crate serde_derive;
 extern crate regex;
 extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 
-pub use framework_detector::FrameworkDetector;
-pub use framework_detector::FrameworkContainer;
 pub use framework_detector::DetectFramework;
+pub use framework_detector::FrameworkContainer;
+pub use framework_detector::FrameworkDetector;
 pub use framework_detector::SourceFile;
+pub use frameworks::Framework;
 
 pub mod dependency;
 pub mod facet;
 pub mod framework_detector;
-pub mod lang;
 pub mod frameworks;
+pub mod lang;
